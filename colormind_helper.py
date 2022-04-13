c150468e8b93f3329b2dce21f88fbf575f973c71
yxia1 <yxia1@babson.edu>
2022-04-13 17:36:12 -0400

    Update colormind_helper.py

--- colormind_helper.py
+++ colormind_helper.py
@@ -1,31 +1,45 @@
 import requests
+
 data = '{"input":[[44,43,44],"N","N","N","N"],"model":"default"}'
-response = requests.post('http://colormind.io/api/', data=data)
+response = requests.post("http://colormind.io/api/", data=data)
 results = response.json()
-generated = results['result']
+generated = results["result"]
 print(generated)
 
+# 1. convert color name into rgb
 def read_txt_to_dict():
     """
     The function open the txt file and read it into a dictionary with the color name as keys and rgb as values
     """
-    f = open('data/colors.txt')
+    f = open("data/colors.txt")
     d = dict()
-    
+
     for line in f:
         row = line.strip()
-        color = row.split(',')
-        color_name= color[0]
+        color = row.split(",")
+        color_name = color[0]
         d[color_name] = list(map(int, color[3:]))
     return d
 
-print(read_txt_to_dict())
+def convert_input(input):
+    """
+    this function takes out the blank space in users' input and makes it lower cases to match with the color dictionary keys
+    """
+    input = input.lower()
+    input = input.replace(" ", "_")
+    return input
 
-def clean_input(user_input):
+#_____________________________________
+# TO DO:
+#_____________________________________
+
+# 2. Generate color palette
+## Communication with API Part
+def clean_API_input(user_input):
     """
     the function takes a user_input of rgb as a list and returns the input that the colormind API accepts
     ie.'{"input":[[44,43,44],"N","N","N","N"],"model":"default"}'
-    string of a dictionary 
+    string of a dictionary
     """
     user_input = list(user_input)
     data = {}
@@ -33,28 +47,40 @@ def clean_input(user_input):
     data.keys = "model"
 
 
-
-def get_palette(clean_input):
+def get_palette(clean_API_input):
     """
     this function takes the clean input and post it to the colormind api to get the generated API
     """
     pass
 
+# 3. where everything comes together
+
+def final(color_name):
+    """
+    return whatever we nee
+    """
+
+
+def main():
+    """
+    You can test all the functions here
+    """
+    color_dict = read_txt_to_dict()
+    print(color_dict[convert_input('green')])
+
+
+
+if __name__ == "__main__":
+    main()
+
+#_____________________________________
+# Unused bits:
+#_____________________________________
+
+
 def rgb_to_hex(rgb):
     """
     This function takes a rgb color and returns its hex code.
     """
-    return f'#''%02x%02x%02x' % rgb
-
-def color_to_rgb(color_name):
-    """
-    takes a color name and returns a rgb as a list
-    reference: https://data-flair.training/blogs/project-in-python-colour-detection/
-    """
-    pass
-
-def get_palette(rgb):
-    """
-    the function takes an input of rgb and return the color palette from colormind
-    """
-    pass
+    # might need this for html
+    return f"#" "%02x%02x%02x" % rgb
