4cee43d1bea551fadd261cd6f08d0158346efb99
yxia1 <yxia1@babson.edu>
2022-04-13 15:29:49 -0400

    Update colormind_helper.py

--- colormind_helper.py
+++ colormind_helper.py
@@ -1,9 +1,26 @@
 import requests
-data = '{"input":[[44,43,44],[90,83,82],"N","N","N"],"model":"default"}'
+data = '{"input":[[44,43,44],"N","N","N","N"],"model":"default"}'
 response = requests.post('http://colormind.io/api/', data=data)
 results = response.json()
 generated = results['result']
 print(generated)
+
+def clean_input(user_input):
+    """
+    the function takes a user_input of rgb as a list and returns the input that the colormind API accepts
+    ie.'{"input":[[44,43,44],"N","N","N","N"],"model":"default"}'
+    string of a dictionary 
+    """
+    user_input = list(user_input)
+    data = {}
+    data.keys = "input"
+    data.keys = "model"
+    data["input"] = 
+
+def get_palette(clean_input):
+    """
+    this function takes the clean input and post it to the colormind api to get the generated API
+    """
 
 def rgb_to_hex(rgb):
     """
@@ -20,4 +37,8 @@ def color_to_rgb(color_name):
     """
     pass
 
-
+def get_palette(rgb):
+    """
+    the function takes an input of rgb 
+    """
+# 
